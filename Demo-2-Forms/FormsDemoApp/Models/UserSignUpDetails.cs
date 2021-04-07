279cb0406fea466e9e7cc44970de37ee00e6bf7e
Darren Neimke <darren.neimke@gmail.com>
2021-04-07 14:40:13 +0930

    Added simple Forms demo code

---
+++ Demo-2-Forms/FormsDemoApp/Models/UserSignUpDetails.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FormsDemoApp.Models
+{
+    public class UserSignUpDetails
+    {
+        [Required]
+        [StringLength(30, ErrorMessage = "Name is too long.")]
+        public string Name { get; set; }
+        
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        
+        [Required]
+        [CompareProperty("Email", 
+            ErrorMessage = "Email and Confirm Email must match")]
+        public string ConfirmEmail { get; set; }
+    }
+}
