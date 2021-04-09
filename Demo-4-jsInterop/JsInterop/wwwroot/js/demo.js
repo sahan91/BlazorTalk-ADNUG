6e1285b9d64014f0b523402c0852144d33636a9b
Darren Neimke <darren.neimke@gmail.com>
2021-04-10 08:25:30 +0930

    Fixed bugs with JsInterop demo

--- Demo-4-jsInterop/JsInterop/wwwroot/js/demo.js
+++ Demo-4-jsInterop/JsInterop/wwwroot/js/demo.js
@@ -3,12 +3,11 @@ export function enableSimple(sel, evt, componentInstance) {
 
     let els = document.querySelectorAll(sel);
 
-    for (var i = 0, l = els.length; i < l; i++) {
+    for (let i = 0, l = els.length; i < l; i++) {
         els[i].addEventListener(evt, function () {
-            alert(`clicked: ${this.id}`);
-
+            let num = this.id;
             const reason = prompt("Why did you click the button?", "No reason")
-            componentInstance.HandleClick(i, reason);
+            componentInstance.invokeMethodAsync('HandleClick', num, reason);
         });
     }
 }
